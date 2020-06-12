65ab77f815752f137c542e09e1ed58d0e496c405
Lakshan Rathnayaka <lrlakshan@gmail.com>
2020-06-12 16:23:05 +0530

    deletted unwanted files

--- src/main/java/com/JNIdemo/JNIapp/Application.java
+++ src/main/java/com/JNIdemo/JNIapp/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application {
 
     static {
         System.load("G:/Simcentric/Sample App/Netbeans/JNIappCdl/dist/libJNIappCdl.dll");
@@ -19,7 +19,6 @@ public class Application implements CommandLineRunner {
     public static void main(String[] args) {
 //        SpringApplication.run(Application.class, args);
         new SpringApplicationBuilder(Application.class).headless(false).run(args);
-////        System.out.println(System.getProperty("java.library.path"));
 //
 //        Figure8Painter figure8 = new Figure8Painter();
         CirclePainter circle = new CirclePainter();
@@ -28,25 +27,9 @@ public class Application implements CommandLineRunner {
         JFrame frame = new JFrame();
         frame.add(circle);
         frame.setVisible(true);
-        frame.setSize(120, 80);
+        frame.setSize(1200, 800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Ball movement");
-//        new Test();
-//        test.nativePrint();
-    }
-
-    @Override
-    public void run(String... strings) throws Exception {
-////        Figure8Painter figure8 = new Figure8Painter();
-//        CirclePainter circle = new CirclePainter();
-////        RaceTrackPainter RT = new RaceTrackPainter();
-////        PathDraw PD = new PathDraw();
-//        JFrame frame = new JFrame();
-//        frame.add(circle);
-//        frame.setVisible(true);
-//        frame.setSize(1200, 800);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.setTitle("Ball movement");
     }
 
 }
